a45d69220aaf2bb6eaca7679d9c51bf0a6972587
Dipinrajc <dipinraj.cheviri@mozanta.com>
2018-08-02 12:11:55 +0530

    https server created and logs removed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const url = require('url');
@@ -12,25 +13,31 @@ const mongoHost = 'mongodb://aniq-mongodb:27018/aniq';
 //const mongoHost = 'mongodb://localhost:27017/aniq';
 const mongoPort = 27017
 
-const httpServer = http.createServer(requestResponseHandler);
+var certsPath = path.join(__dirname, 'certs', 'server');
+options = {
+  key: fs.readFileSync(path.join(certsPath, 'my-server.key.pem'))
+, cert: fs.readFileSync(path.join(certsPath, 'my-server.crt.pem'))
+, requestCert: false
+, rejectUnauthorized: true
+};
+
+const httpServer = https.createServer(options,requestResponseHandler);
+
+
+//const httpServer = http.createServer(requestResponseHandler);
 httpServer.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 function requestResponseHandler(req, res) {
-  console.log(`Request came: ${req.url}`);
-  console.log(`Request came: ${req}`);
   if (req.method === 'GET') {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
-    console.log('Key is ' + query.apiKey);
     let callback = query.callback;
     checkApiKey(req, res, query.apiKey).then((result) => {
       if (req.url.indexOf('get-rating') > -1) {
-        console.log(`Get Rating`);
         getRating(req, res, query.productId, query.callback);
       } else if (req.url.indexOf('update-rating') > -1) {
-        console.log(`Update Rating`);
         updateRating(req, res, query.productId, query.callback);
       }
     }).catch((error) => {
@@ -52,16 +59,11 @@ function getRating(req, res, productId, callback) {
   MongoClient.connect(mongoHost, function (err, client) {
     if (err) throw err;
     const db = client.db('aniq');
-    console.log("Connected to aniq database!");
     var rating = 0;
     findByProductId(db, productId).then((result) => {
       if (null != result) {
-        console.log("Found product with productId " + productId);
         rating = result.rating | 0;
-      } else {
-        console.log("Not Found product with productId " + productId);
       }
-      console.log("Current rating is " + rating);
       content = { 'success': true, 'rating': rating };
       dataToSent = callback + "(" + JSON.stringify(content) + ")";
       client.close();
@@ -74,23 +76,17 @@ function updateRating(req, res, productId, callback) {
   MongoClient.connect(mongoHost, function (err, client) {
     if (err) throw err;
     const db = client.db('aniq');
-    console.log("Connected to aniq database!");
     var rating = 0;
     findByProductId(db, productId).then((result) => {
       if (null != result) {
-        console.log("Found product with productId " + productId);
         rating = result.rating | 0;
       } else {
-        console.log("Not Found product with productId " + productId);
         result = {};
         result.productId = productId;
       }
-      console.log("Current rating is " + rating);
       rating++;
-      console.log("Updated rating is " + rating);
       result.rating = rating;
       updateProductRating(db, result).then((result) => {
-        console.log("Result is " + result);
         content = { 'success': true, 'rating': rating };
         dataToSent = callback + "(" + JSON.stringify(content) + ")";
         client.close();
@@ -103,7 +99,6 @@ function updateRating(req, res, productId, callback) {
 const findByProductId = (db, productId) => {
   return new Promise(function (resolve, reject) {
     const collection = db.collection('products');
-    console.log('Fetching products collection');
     collection.findOne({ "productId": productId }, function (err, result) {
       if (err) reject(err);
       resolve(result);
@@ -114,11 +109,8 @@ const findByProductId = (db, productId) => {
 const updateProductRating = (db, data) => {
   return new Promise(function (resolve, reject) {
     const collection = db.collection('products');
-    console.log('Updating products collection');
     collection.save(data, function (err, result) {
-      console.log('Error ' + err);
       if (err) reject(err);
-      console.log('Updated Successfully');
       resolve(result);
     });
   });
@@ -129,17 +121,12 @@ function checkApiKey(req, res, apiKey) {
     MongoClient.connect(mongoHost, function (err, client) {
       if (err) throw err;
       const db = client.db('aniq');
-      console.log("Connected to aniq database!");
       var enabled = false;
       const collection = db.collection('apikeys');
-      console.log('Fetching apikeys collection');
       collection.findOne({ "apiKey": apiKey }, function (err, result) {
         if (err) reject(err);
         if (null != result) {
-          console.log("Found apikey");
           enabled = result.enabled | false;
-        } else {
-          console.log("Not Found apikey");
         }
         if (!enabled) {
           reject(0);
