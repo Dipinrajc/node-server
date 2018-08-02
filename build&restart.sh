2dda96f29be7ca485fe1c9a7b3e079fe243f25a9
Dipinrajc <dipinraj.cheviri@mozanta.com>
2018-08-02 13:07:02 +0530

    Update build&restart.sh

--- build&restart.sh
+++ build&restart.sh
@@ -3,18 +3,18 @@ echo "################################################"
 echo "Build server-app image"
 echo "################################################"
 
-docker build -t server-app
+sudo docker build -t server-app .
 
 echo "################################################"
 echo "Deletes the old dangling images"
 echo "################################################"
 
-docker 2>/dev/null rmi -f $(docker images -q --filter "dangling=true")
-docker images --quiet --filter=dangling=true | xargs --no-run-if-empty docker rmi -f
+sudo docker 2>/dev/null rmi -f $(docker images -q --filter "dangling=true")
+sudo docker images --quiet --filter=dangling=true | xargs --no-run-if-empty docker rmi -f
 
 echo "################################################"
 echo "Run Images"
 echo "################################################"
 
-docker-compose rm -f
-docker-compose up -d
+sudo docker-compose rm -f
+sudo docker-compose up -d
